4feb49db46ff621e6a675ee7c39b992dfd177377
C3S4R31 <ing.cesaravilat@gmail.com>
2022-12-06 18:01:39 -0500

    InstaYa backend-2

--- index.js
+++ index.js
@@ -31,11 +31,6 @@ const server = app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
 
-//404 Error
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
 app.use(function (err, req, res, next) {
     console.error(err.message);
     if (!err.statusCode) err.statusCode = 500;
